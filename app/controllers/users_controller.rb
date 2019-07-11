f5cfbf6593f01cd0d46e28ca019df49fd6903709
Carlo <carlomjavier@gmail.com>
2019-07-11 23:44:32 +0000

    implement login/logout feature after weird bug

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,19 +3,21 @@ class UsersController < ApplicationController
     def new
     end
 
-    def create 
+    def create
         user = User.new(user_params)
+ 
+
         if user.save
-            session[:user_id] = user_id
-            redirect to '/'
+          session[:user_id] = user.id
+          redirect_to '/'
         else
-            redirect to '/register'
+          redirect_to '/signup'
         end
     end
 
     private
 
     def user_params
-        params.require(:user).permit(:name, :email, :password, :password_confirmation)
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -18,12 +18,14 @@ Rails.application.routes.draw do
     resources :categories, only: [:index, :new, :create]
   end
 
-  get '/login' => 'sessions#new'
-  post '/login' => 'sessions#create'
-  get '/logout' => 'sessions#destroy'
+  get '/login' => 'session#new'
+  post '/login' => 'session#create'
+  get '/logout' => 'session#destroy'
 
   get '/signup' => 'users#new'
   post '/users' => 'users#create'
+
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
