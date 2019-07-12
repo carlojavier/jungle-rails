5d3bcebf8cef47a421b4d5f14d4cdae643bc3e98
Carlo <carlomjavier@gmail.com>
2019-07-12 23:07:49 +0000

    hardcode reviews in seeds file

--- db/migrate/20190712205955_create_reviews.rb
+++ db/migrate/20190712205955_create_reviews.rb
@@ -1,6 +1,10 @@
 class CreateReviews < ActiveRecord::Migration
   def change
     create_table :reviews do |t|
+      t.references :product
+      t.references :user
+      t.text :description
+      t.integer :rating
 
       t.timestamps null: false
     end
--- db/seeds.rb
+++ db/seeds.rb
@@ -132,5 +132,10 @@ cat3.products.create!({
   price: 2_483.75
 })
 
+Review.create!(product_id:1, user_id:1, description:"It's too tight", rating:2)
+Review.create!(product_id:1, user_id:2, description:"Oof", rating:1)
+Review.create!(product_id:2, user_id:2, description:"I'm satisfied... I guess that's good?", rating:3)
+Review.create!(product_id:3, user_id:3, description:"Love it more than myself!", rating:5)
+
 
 puts "DONE!"
