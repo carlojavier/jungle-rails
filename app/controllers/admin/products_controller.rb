561baa4463a47dc68a929b2b60802fc8d1ff377d
Carlo <carlomjavier@gmail.com>
2019-07-12 06:27:56 +0000

    implement HTTP basic auth for admin

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,5 +1,7 @@
 sclass Admin::ProductsController < ApplicationController
 
+  http_basic_authenticate_with name: ENV['ADMIN_USER'], password: ENV['ADMIN_PASSWORD']
+
   def index
     @products = Product.order(id: :desc).all
   end
